0f1aaace1aa7d38c1dc413013996e1547ebc795a
heifeise <3164396689@qq.com>
2025-02-11 01:32:23 +0800

    合并性提交

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,10 +1,19 @@
+--标签栏
 return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
+		vim.opt.termguicolors=true
 		require("bufferline").setup({
-			options = {
+		options = {
+mousemoveevent=enabled,
+        	hover = {
+            	enabled = true,
+            	delay = 200,
+				
+            	reveal = {'close'}
+        		},
 				mode = "buffers", -- 显示缓冲区
 				numbers = "ordinal", -- 显示缓冲区编号
 				diagnostics = "nvim_lsp", -- 显示 LSP 诊断
