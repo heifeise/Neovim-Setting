cf115fb233ab93af6b2ddc5d5bc5f16211164381
heifeise <3164396689@qq.com>
2025-04-17 01:08:38 +0800

    commit before pull,but this branch is out

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,7 +2,7 @@
 return {
 	"akinsho/bufferline.nvim",
 	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
+	dependencies = "nvim-tree/nvim-web-devicons", 
 	config = function()
 		vim.opt.termguicolors = true
 		vim.opt.mousemoveevent = true
